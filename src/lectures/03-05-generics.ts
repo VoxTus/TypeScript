d9b40b88b1bf7636cd9c0572bfbda821bfb0b32e
VoxTus <votu10032003@gmail.com>
2024-05-29 15:48:07 +0700

    03-05-upadte

--- src/lectures/03-05-generics.ts
+++ src/lectures/03-05-generics.ts
@@ -2,7 +2,6 @@ interface List<T,> {
     length : number;
     [index: number]: T;
 }
-
 const nnumberList :List<number> =[1, 2, 3, 4, 5];
 const wordLists :List<string> =['Vox' ,'Tu'];
 
@@ -10,5 +9,26 @@ interface Student{
     id: number;
     name: string;
 }
-
 // const studentList: List<Student> = [{id: 1, name: 'Tu'}]
+
+
+// keyof operator
+
+// console.log(typeof 'easy frontend'); // 'string'
+// console.log(typeof 123); // 'number'
+// console.log(typeof false); // 'boolean'
+// console.log(typeof {}); // 'object'
+// console.log(typeof []); // 'object'
+// console.log(typeof function() {}); // 'function'
+// console.log(typeof window !== 'undefined'); // true on client, false on server
+
+// Mapped types
+// Dùng index signature để định nghĩa kiểu dữ liệu có props lấy từ kiểu dữ liệu khác.
+
+// interface Student {
+// id: number;
+// name: string;
+// }
+// type MappedTypes = {
+// [Key in keyof Student]: boolean;
+// };
